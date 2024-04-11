26af767fc7ed50cd4167e0d45654b59699360dc0
newLisa2024 <165168788+newLisa2024@users.noreply.github.com>
2024-04-11 13:17:28 +0300

    add all two objects

--- test.py
+++ test.py
@@ -3,8 +3,18 @@ pygame.init()
 
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
-# screen = pygame.display.set_mode((640, 480), 0, 32)
+
 pygame.display.set_caption('тестовый проект')
+
+image1 = pygame.image.load('picPython1.png',)
+image1 = pygame.transform.scale(image1, (100, 100))
+image_rect1 = image1.get_rect()
+
+image2 = pygame.image.load('pycharm.png',)
+image2 = pygame.transform.scale(image2, (100, 100))
+image_rect2 = image1.get_rect()
+
+# speed = 5
 
 run = True
 
@@ -12,8 +22,26 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            image_rect1.x = mouseX
+            image_rect1.y = mouseY
 
-    screen.fill((255, 34, 250))
+    if image_rect1.colliderect(image_rect2):
+        print('Произошло столкновение')
+
+   # keys = pygame.key.get_pressed()
+   # if keys[pygame.K_LEFT]:
+   #    image_rect.x -= speed
+   # if keys[pygame.K_RIGHT]:
+   #     image_rect.x += speed
+   # if keys[pygame.K_UP]:
+   #     image_rect.y -= speed
+   # if keys[pygame.K_DOWN]:
+   #     image_rect.y += speed
+    screen.fill((0, 0, 0))
+    screen.blit(image1, image_rect1)
+    screen.blit(image2, image_rect2)
     pygame.display.flip()
 
 pygame.quit()
